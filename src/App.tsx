4338040cb10979db6540cd3b3e5d62b8ab180e8f
Anton <ibeliev@gmail.com>
2026-03-10 17:03:08 -0700

    feat: add http component and API fetching logic to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,49 @@
+import { useEffect, useState, type ReactNode } from 'react';
+
+
+import BlogPosts, { type BlogPost } from './components/BlogPosts.tsx'
+import { get } from './components/util/http';
+import fetchingImg from './assets/data-fetching.png';
+
+type RawDataBlogPost = {
+  id:number;
+  userId:number;
+  title:string;
+  body:string;
+}
+
 function App() {
-  return <h1>Data Fetching!</h1>;
+
+  const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+
+  useEffect(() => {
+    async function fetchPosts() {
+
+      const data = (await get('https://jsonplaceholder.typicode.com/posts')) as RawDataBlogPost[];
+
+      const blogPosts: BlogPost[] = data.map(rawPost =>{
+        return {
+          id: rawPost.id,
+          title: rawPost.title,
+          text: rawPost.body
+        }
+      });
+
+      setFetchedPosts(blogPosts);
+    }
+    fetchPosts();
+  }, []);
+
+  let content: ReactNode;
+
+if(fetchedPosts){
+  content= <BlogPosts posts={fetchedPosts}/>
+}
+
+  return <main>
+    <img src={fetchingImg} alt='image'/>
+    {content}
+  </main>;
 }
 
 export default App;
