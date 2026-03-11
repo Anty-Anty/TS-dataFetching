01fb9ce7d4dace5c48ce8b98eda71352197c1bc4
Anton <ibeliev@gmail.com>
2026-03-10 17:31:47 -0700

    Add error handling logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,50 +4,63 @@ import { useEffect, useState, type ReactNode } from 'react';
 import BlogPosts, { type BlogPost } from './components/BlogPosts.tsx'
 import { get } from './components/util/http';
 import fetchingImg from './assets/data-fetching.png';
+import ErrorMessage from './components/ErrorMessage.tsx';
 
 type RawDataBlogPost = {
-  id:number;
-  userId:number;
-  title:string;
-  body:string;
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
 }
 
 function App() {
 
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
   const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState<string>();
 
   useEffect(() => {
     async function fetchPosts() {
       setIsFetching(true);
-      const data = (await get('https://jsonplaceholder.typicode.com/posts')) as RawDataBlogPost[];
+      try {
+        const data = (await get('https://jsonplaceholder.typicode.com/posts')) as RawDataBlogPost[];
+        const blogPosts: BlogPost[] = data.map(rawPost => {
+          return {
+            id: rawPost.id,
+            title: rawPost.title,
+            text: rawPost.body
 
-      const blogPosts: BlogPost[] = data.map(rawPost =>{
-        return {
-          id: rawPost.id,
-          title: rawPost.title,
-          text: rawPost.body
+          }
+        });
+        setFetchedPosts(blogPosts);
+      } catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
         }
-      });
+        // setError('Failed to fetch posts!');
+      }
 
       setIsFetching(false);
-      setFetchedPosts(blogPosts);
     }
     fetchPosts();
   }, []);
 
   let content: ReactNode;
 
-if(fetchedPosts){
-  content= <BlogPosts posts={fetchedPosts}/>
-}
+  if (error) {
+    content = <ErrorMessage text={error} />
+  }
 
-if (isFetching){
-  content = <p id="loading-fallback">Fetching posts...</p>
-}
+  if (fetchedPosts) {
+    content = <BlogPosts posts={fetchedPosts} />
+  }
+
+  if (isFetching) {
+    content = <p id="loading-fallback">Fetching posts...</p>
+  }
 
   return <main>
-    <img src={fetchingImg} alt='image'/>
+    <img src={fetchingImg} alt='image' />
     {content}
   </main>;
 }
